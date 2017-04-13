238f3b77c6c9482b703c554acc8f9d404ca66ab6
Jeremy Morrell <jmorrell@heroku.com>
2017-04-13 15:52:46 -0700

    bump

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer();
+
+server.on('request', dispatch);
+
+function dispatch(req, res) {
+  res.on('finish', () => {
+    req.socket.end();
+  });
+
   res.statusCode = 404;
   res.setHeader('content-type', 'application/json; charset=utf-8');
   res.setHeader('cache-control', 'no-cache');
   res.setHeader('content-length', '38');
   res.setHeader('vary', 'accept-encoding');
-  res.end(`{"statusCode":404,"error":"Not Found"}`);
-});
+  res.write(`{"statusCode":404,"error":"Not Found"}`);
+  res.end();
+}
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
