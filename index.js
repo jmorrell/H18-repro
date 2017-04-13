a6015f1ce503d183ce94101710052322f0b1a186
Jeremy Morrell <jmorrell@heroku.com>
2017-04-13 16:53:54 -0700

    bump

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const server = http.createServer();
 
 server.on('request', dispatch);
 
+
 function dispatch(req, res) {
+  req.socket.setTimeout(0);
 
   res.statusCode = 404;
   res.setHeader('content-type', 'application/json; charset=utf-8');
   res.setHeader('cache-control', 'no-cache');
   res.setHeader('content-length', '38');
   res.setHeader('vary', 'accept-encoding');
-  res.write('{"statusCode":404,');
-  res.write('error":"Not Found"}');
+  res.write('{"statusCode":404,"error":"Not Found"}');
   res.on('finish', () => {
     req.socket.end();
   });
-  // res.end();
+  res.end();
 }
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
